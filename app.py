bc5aba77be08031cc810fcafd3f8810dd5d2820c
runzhouwu-git <babowu124@gmail.com>
2025-05-26 22:36:56 +0800

    初始化

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, send_from_directory
+import json
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/poem')
+def get_poem():
+    with open('poem.json', 'r', encoding='utf-8') as f:
+        poem_list = json.load(f)
+    return jsonify(random.choice(poem_list))
+
+
+@app.route('/audio/<filename>')
+def get_audio(filename):
+    return send_from_directory('static', filename)
+
+
+if __name__ == '__main__':
+    app.run()
